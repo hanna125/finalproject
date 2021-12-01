929a001cc865bcaacba8548616b5729d05cb52b6
hanna125 <93460227+hanna125@users.noreply.github.com>
2021-12-01 15:22:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
 
+st.write("""
+# Bitcoin Self Flagellation App
+
+This app is for those wannabe crypto bros (and sisses) who want to punish themselves by knowing how much $$ you could have 
+today if you had bought Bitcoin on a certain date. 
+
+Select the following two inputs:
+- Date You Wish You Would Have Bought Bitcoin
+- USD Amount You Wish You Would Have Invested
+""")
+st.write('---')
+
+st.image('BTC.jpg', use_column_width=True)
