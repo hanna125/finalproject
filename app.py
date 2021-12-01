b0c7217f134378da712291ceecf498ca19304815
hanna125 <93460227+hanna125@users.noreply.github.com>
2021-12-01 15:36:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -16,3 +16,17 @@ Select the following two inputs:
 st.write('---')
 
 st.image('BTC.jpg', use_column_width=True)
+
+#get data
+btc = read.csv('BTC-USD.csv')
+ 
+# user input
+st.write('''# Choose Date and Amount''')
+today = datetime.utcnow().date()
+previous_day = today - timedelta(days=1)
+HIST_DATE = st.date_input("Date: ", value=previous_day, min_value=datetime(2014,1,1), max_value=previous_day)
+ORG_USD = st.number_input("USD Amount: ", min_value=1, max_value=999999999)
+
+#date reformatting
+HIST_DATE_REFORMAT = HIST_DATE.strftime("%d-%m-%Y")
+HIST_DATE_datetime = datetime.strptime(HIST_DATE_REFORMAT,"%d-%m-%Y")
