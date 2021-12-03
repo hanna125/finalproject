54cb50a9450fae6ea1b13646649c8449bddb9274
hanna125 <93460227+hanna125@users.noreply.github.com>
2021-12-03 11:43:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -72,4 +72,4 @@ st.write(abs(round(usd_diff,2)))
 
 historical_prices = btc.loc[:,["Date","Close"]]
 
-st.write(historical_prices.set_index('Date').plot())
+historical_prices.set_index('Date').plot()
